3df2288d08784bfc998539842f942a4a7716b9e9
Aastha <aastha@Aasthas-MacBook-Air.local>
2026-07-16 20:14:23 +0530

    style: make entire 'X days until youre home' title pulsate every second

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -268,9 +268,9 @@ function LiveCountdownHero({ daysUntilHome, percentComplete }: { daysUntilHome: 
       <div className="ch-blob ch-blob-1" />
       <div className="ch-blob ch-blob-2" />
       
-      <div className="ch-title">
+      <div key={timeLeft.seconds} className="ch-title">
         {timeLeft.days} Days Until You're Home
-        <svg key={timeLeft.seconds} className="ch-heart-svg" viewBox="0 0 32 29.6">
+        <svg className="ch-heart-svg" viewBox="0 0 32 29.6">
           <path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2 c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z" />
         </svg>
       </div>
